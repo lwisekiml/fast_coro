33dd791d04e23ecc6447beaab78367cb1ad71128
wisekim <lwisekiml@naver.com>
2024-02-02 08:59:34 +0900

    BaseController and BaseErrorController (04. ControllerAdvice(2))

--- src/main/java/com/uno/getinline/Controller/BaseController.java
+++ src/main/java/com/uno/getinline/Controller/BaseController.java
@@ -3,18 +3,14 @@ package com.uno.getinline.Controller;
 import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-public class BaseController implements ErrorController { // ErrorController를 해야 error 권한을 가져옴
+public class BaseController { // ErrorController를 해야 error 권한을 가져옴
 
     @GetMapping("/")
-    public String root() {
-        return "index";
+    public String root() throws Exception{
+        throw new Exception("테스트");
+//        return "index";
     }
 
-    @RequestMapping("/error")
-    public String error() {
-        return "error";
-    }
 }
