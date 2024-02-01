00f8c8d4de5f603be831c36a89549de9c9a94544
wisekim <lwisekiml@naver.com>
2024-02-01 23:26:12 +0900

    스프링 부트 기본 에러 응답 부분 테스트

--- src/main/java/com/uno/getinline/Controller/BaseController.java
+++ src/main/java/com/uno/getinline/Controller/BaseController.java
@@ -6,15 +6,16 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-public class BaseController implements ErrorController { // ErrorController를 해야 error 권한을 가져옴
+public class BaseController { // ErrorController를 해야 error 권한을 가져옴
 
     @GetMapping("/")
-    public String root() {
-        return "index";
+    public String root() throws Exception {
+        throw new Exception("테스트");
+//        return "index";
     }
 
-    @RequestMapping("/error")
-    public String error() {
-        return "error";
-    }
+//    @RequestMapping("/error")
+//    public String error() {
+//        return "error";
+//    }
 }
