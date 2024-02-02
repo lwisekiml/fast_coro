0e7870fb9c26a0b95b85657f3ed4027070e56baf
wisekim <lwisekiml@naver.com>
2024-02-02 10:01:22 +0900

    test 원복

--- src/main/java/com/uno/getinline/Controller/BaseController.java
+++ src/main/java/com/uno/getinline/Controller/BaseController.java
@@ -1,6 +1,5 @@
 package com.uno.getinline.Controller;
 
-import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
@@ -8,9 +7,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class BaseController { // ErrorController를 해야 error 권한을 가져옴
 
     @GetMapping("/")
-    public String root() throws Exception{
-        throw new Exception("테스트");
-//        return "index";
+    public String root() {
+        return "index";
     }
 
 }
--- src/main/java/com/uno/getinline/Controller/api/APIEventController.java
+++ src/main/java/com/uno/getinline/Controller/api/APIEventController.java
@@ -1,10 +1,5 @@
 package com.uno.getinline.Controller.api;
 
-import com.uno.getinline.constant.ErrorCode;
-import com.uno.getinline.dto.APIErrorResponse;
-import com.uno.getinline.exception.GeneralException;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -15,14 +10,12 @@ public class APIEventController {
 
     @GetMapping("/events")
     public List<String> getEvents() {
-        throw new GeneralException("테스트 메시지"); // 지정한 에러
-//        return List.of("event1", "event2");
+        return List.of("event1", "event2");
     }
 
     @PostMapping("/events")
     public Boolean createEvent() {
-        throw new RuntimeException("runtime 테스트 메시지"); // 지정하지 않은 에러 -> 공통에러 페이지로 넘어가서 적당한 해동 함(에러 페이지를 view로 보여줌)
-//        return true;
+        return true;
     }
 
     @GetMapping("/events/{eventId}")
@@ -40,17 +33,4 @@ public class APIEventController {
         return true;
     }
 
-    @ExceptionHandler
-    public ResponseEntity<APIErrorResponse> general(GeneralException e) { // 위 메소드에서 exception이 터지면 이 메소드에서 잡는다.
-        ErrorCode errorCode = e.getErrorCode();
-        HttpStatus status = errorCode.isClientSideError() ?
-                HttpStatus.BAD_REQUEST :
-                HttpStatus.INTERNAL_SERVER_ERROR;
-
-        return ResponseEntity
-                .status(status)
-                .body(APIErrorResponse.of(
-                        false, errorCode, errorCode.getMessage(e)
-                ));
-    }
 }
