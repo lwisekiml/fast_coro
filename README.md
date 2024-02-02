08d97f39c77cfbb42134e706f7f7d538326410ce
wisekim <lwisekiml@naver.com>
2024-02-02 10:00:50 +0900

    implement - api controller advice

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # fast_coro
+
+get, post  
+>localhost:8080/api/events  
+localhost:8080
+
---
+++ src/main/java/com/uno/getinline/Controller/error/APIExceptionHandler.java
@@ -0,0 +1,39 @@
+package com.uno.getinline.Controller.error;
+
+import com.uno.getinline.constant.ErrorCode;
+import com.uno.getinline.dto.APIErrorResponse;
+import com.uno.getinline.exception.GeneralException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice(annotations = RestController.class) // RestController annotation을 쓰는 것만 대상
+public class APIExceptionHandler {
+
+    @ExceptionHandler
+    public ResponseEntity<APIErrorResponse> general(GeneralException e) {
+        ErrorCode errorCode = e.getErrorCode();
+        HttpStatus status = errorCode.isClientSideError() ?
+                HttpStatus.BAD_REQUEST :
+                HttpStatus.INTERNAL_SERVER_ERROR;
+
+        return ResponseEntity
+                .status(status)
+                .body(APIErrorResponse.of(false, errorCode, errorCode.getMessage(e)
+                ));
+    }
+
+    public ResponseEntity<APIErrorResponse> exception(GeneralException e) {
+        ErrorCode errorCode = ErrorCode.INTERNAL_ERROR;
+        HttpStatus status = HttpStatus.INTERNAL_SERVER_ERROR;
+
+        return ResponseEntity
+                .status(status)
+                .body(APIErrorResponse.of(
+                        false, errorCode, errorCode.getMessage(e)
+                ));
+    }
+
+}
