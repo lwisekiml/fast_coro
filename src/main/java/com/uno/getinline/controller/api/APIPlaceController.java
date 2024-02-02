57ef22e0b37b3c954cad4e2c4523b4b7104c44fc
wisekim <lwisekiml@naver.com>
2024-02-02 13:45:03 +0900

    APIPlaceController (05. 컨트롤러 테스트(2))

--- src/main/java/com/uno/getinline/controller/api/APIPlaceController.java
+++ src/main/java/com/uno/getinline/controller/api/APIPlaceController.java
@@ -1,5 +1,8 @@
 package com.uno.getinline.controller.api;
 
+import com.uno.getinline.constant.PlaceType;
+import com.uno.getinline.dto.APIDataResponse;
+import com.uno.getinline.dto.PlaceDTO;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -9,8 +12,15 @@ import java.util.List;
 public class APIPlaceController {
 
     @GetMapping("/places")
-    public List<String> getPlaces() {
-        return List.of("place1", "place2");
+    public APIDataResponse<List<PlaceDTO>> getPlaces() {
+        return APIDataResponse.of(List.of(PlaceDTO.of(
+                PlaceType.COMMON,
+                "랄라배드민턴장",
+                "서울시 강남구 강남대로 1234",
+                "010-1234-5678",
+                30,
+                "신장개업"
+        )));
     }
 
     @PostMapping("/places")
@@ -18,9 +28,21 @@ public class APIPlaceController {
         return true;
     }
 
-    @GetMapping("/places/{placeId}")
-    public String getPlace(@PathVariable Integer placeId) {
-        return "place " + placeId;
+    @GetMapping("/places/{placeId}") // 단일 장소
+    public APIDataResponse<PlaceDTO> getPlace(@PathVariable Integer placeId) {
+        // 테스트를 위한 코드
+        if (placeId.equals(2)) {
+            return APIDataResponse.of(null);
+        }
+
+        return APIDataResponse.of(PlaceDTO.of(
+                PlaceType.COMMON,
+                "랄라배드민턴장",
+                "서울시 강남구 강남대로 1234",
+                "010-1234-5678",
+                30,
+                "신장개업"
+        ));
     }
 
     @PutMapping("/places/{placeId}")
