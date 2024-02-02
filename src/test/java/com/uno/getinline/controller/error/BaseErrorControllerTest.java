d959e19045c635681359790b2427f1464034915f
wisekim <lwisekiml@naver.com>
2024-02-02 11:27:42 +0900

    view error test (05. 컨트롤러 테스트(2))

---
+++ src/test/java/com/uno/getinline/controller/error/BaseErrorControllerTest.java
@@ -0,0 +1,32 @@
+package com.uno.getinline.controller.error;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(BaseErrorController.class)
+class BaseErrorControllerTest {
+
+    private final MockMvc mvc;
+
+    public BaseErrorControllerTest(@Autowired MockMvc mvc) {
+        this.mvc = mvc;
+    }
+
+    @DisplayName("[view][GET] 에러 페이지 - 페이지 없음")
+    @Test
+    void givenWrongURI_whenRequestingRootPage_thenReturn404() throws Exception {
+        // given
+
+        // when & then
+        mvc.perform(get("/wrong-url"))
+                .andExpect(status().isNotFound())
+                .andDo(print());
+    }
+}
