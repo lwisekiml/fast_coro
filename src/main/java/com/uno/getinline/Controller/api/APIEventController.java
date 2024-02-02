0d93ca9f9ee275756b0e100c33cac57acd23bdb3
wisekim <lwisekiml@naver.com>
2024-02-02 10:15:21 +0900

    test 원복

--- src/main/java/com/uno/getinline/Controller/api/APIEventController.java
+++ src/main/java/com/uno/getinline/Controller/api/APIEventController.java
@@ -1,7 +1,5 @@
 package com.uno.getinline.Controller.api;
 
-import com.uno.getinline.exception.GeneralException;
-import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -11,21 +9,18 @@ import java.util.List;
 public class APIEventController {
 
     @GetMapping("/events")
-    public List<String> getEvents() throws Exception {
-        throw new HttpRequestMethodNotSupportedException("스프링 에러 테스트");
-//        return List.of("event1", "event2");
+    public List<String> getEvents() {
+        return List.of("event1", "event2");
     }
 
     @PostMapping("/events")
     public Boolean createEvent() {
-        throw new GeneralException("general exception!!");
-//        return true;
+        return true;
     }
 
     @GetMapping("/events/{eventId}")
     public String getEvent(@PathVariable Integer eventId) {
-        throw new RuntimeException("run time");
-//        return "event " + eventId;
+        return "event " + eventId;
     }
 
     @PutMapping("/events/{eventId}")
