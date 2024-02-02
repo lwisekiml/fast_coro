ee9c1d5fcb0fb8e33b7b87079c1788ef4361dbb0
wisekim <lwisekiml@naver.com>
2024-02-02 09:35:40 +0900

    implement - view controller advice

---
+++ src/main/java/com/uno/getinline/Controller/error/BaseExceptionHandler.java
@@ -0,0 +1,50 @@
+package com.uno.getinline.Controller.error;
+
+import com.uno.getinline.constant.ErrorCode;
+import com.uno.getinline.exception.GeneralException;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.Map;
+
+// 전체 컨트롤러 동작 관리(view에 대한 컨트롤 어드바이스)
+@ControllerAdvice
+public class BaseExceptionHandler {
+
+    @ExceptionHandler // general exception 터졌을 때
+    public ModelAndView general(GeneralException e) {
+        ErrorCode errorCode = e.getErrorCode();
+        HttpStatus status = errorCode.isClientSideError() ?
+                HttpStatus.BAD_REQUEST :
+                HttpStatus.INTERNAL_SERVER_ERROR;
+
+        return new ModelAndView(
+                "error",
+                Map.of(
+                        "statusCode", status.value(),
+                        "errorCode", errorCode,
+                        "message", errorCode.getMessage(e)
+                ),
+                status
+        );
+    }
+
+    @ExceptionHandler // 예상 못한 일반적인 에러
+    public ModelAndView exception(Exception e) {
+        ErrorCode errorCode = ErrorCode.INTERNAL_ERROR;
+        HttpStatus status = HttpStatus.INTERNAL_SERVER_ERROR;
+
+        return new ModelAndView(
+                "error",
+                Map.of(
+                        "statusCode", status.value(),
+                        "errorCode", errorCode,
+                        "message", errorCode.getMessage(e)
+                ),
+                status
+        );
+    }
+
+}
