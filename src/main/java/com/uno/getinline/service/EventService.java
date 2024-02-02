5bacc0d01ee499585c73c5cae835120f6f0c9085
wisekim <lwisekiml@naver.com>
2024-02-02 15:12:10 +0900

    01. @service 이해(1)

---
+++ src/main/java/com/uno/getinline/service/EventService.java
@@ -0,0 +1,41 @@
+package com.uno.getinline.service;
+
+import com.uno.getinline.constant.EventStatus;
+import com.uno.getinline.dto.EventDTO;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class EventService {
+
+    private final EventRepository eventRepository;
+
+    public List<EventDTO> getEvents(
+            Long placeId,
+            String eventName,
+            EventStatus eventStatus,
+            LocalDateTime eventStartDatetime,
+            LocalDateTime eventEndDatetime
+    ) {
+        return eventRepository.findEvents();
+    }
+
+    public Optional<EventDTO> getEvent(Long eventId) {
+        return Optional.empty();
+    }
+
+    public boolean createEvent(EventDTO eventDTO) {
+        return true;
+    }
+
+    public boolean modifyEvent(Long eventId, EventDTO dto) {
+        return true;
+    }
+
+    public boolean removeEvent(Long eventId) {
+        return true;
+    }
+}
