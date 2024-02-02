855cdb87364c910f86bd332e1f0d400ba8b5013e
wisekim <lwisekiml@naver.com>
2024-02-02 11:23:22 +0900

    @WebMvcTest (05. 컨트롤러 테스트(2))

--- src/test/java/com/uno/getinline/controller/BaseControllerTest.java
+++ src/test/java/com/uno/getinline/controller/BaseControllerTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
@@ -13,8 +14,8 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
-@AutoConfigureMockMvc
-@SpringBootTest
+//@WebMvcTest // 모든 컨트롤러를 읽는다.
+@WebMvcTest(BaseController.class) // 해당 컨트롤러를 읽는다.
 class BaseControllerTest {
 
     private final MockMvc mvc;
